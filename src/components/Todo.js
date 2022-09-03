3332b3fff6d11ba94c4692d0e32002fc7f8ce08b
jkyoon-dev <jkyoon.dev@gmail.com>
2022-09-03 11:48:36 +0900

    Todo list init

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+const Todo = () => {
+  return (
+    <div className='card'>
+      <h2>Todos</h2>
+      <div className='actions'>
+        <button className='btn'>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
